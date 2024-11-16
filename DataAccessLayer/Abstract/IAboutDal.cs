0ca056641ee07dd005e7495d9d8e0819ee9c8db3
selindil <0selindil@gmail.com>
2024-11-16 16:13:02 +0300

    ...

--- DataAccessLayer/Abstract/IAboutDal.cs
+++ DataAccessLayer/Abstract/IAboutDal.cs
@@ -7,9 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface IGuideDal:IGenericDal<Guide>
+    public interface IAboutDal:IGenericDal<About>
     {
-   
-
     }
 }
