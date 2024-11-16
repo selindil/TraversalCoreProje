e4a35a8c6c9dbfbb1c47c11435c3203e53ac75ed
selindil <0selindil@gmail.com>
2024-11-16 20:10:04 +0300

    ...

--- BusinessLayer/Concrete/AboutManager.cs
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class AboutManager
+    public class AboutManager : IAboutService
     {
+        IAboutDal _aboutDal;
+        public AboutManager(IAboutDal aboutDal)
+        {
+                _aboutDal=aboutDal;
+        }
+        public About GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> GetList()
+        {
+           return _aboutDal.GetList();
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutDal.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+           _aboutDal.Delete(t);
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
     }
 }
