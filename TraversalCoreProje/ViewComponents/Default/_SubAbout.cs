0e83c34d1e079123c867ebdc2df89ce3be2c50c0
selindil <0selindil@gmail.com>
2024-12-17 19:04:53 +0300

    ...

---
+++ TraversalCoreProje/ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _SubAbout : ViewComponent
+    {
+        SubAboutManager subAboutManager = new SubAboutManager(new EfSubAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = subAboutManager.GetList();
+            return View(values); 
+        }
+    }
+}
