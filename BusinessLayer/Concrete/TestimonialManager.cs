728783059bc0b8922c6e6b8f582a9ed628242f24
selindil <0selindil@gmail.com>
2024-12-28 16:11:17 +0300

    ..

--- BusinessLayer/Concrete/TestimonialManager.cs
+++ BusinessLayer/Concrete/TestimonialManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,36 @@ namespace BusinessLayer.Concrete
 {
     public class TestimonialManager : ITestimonialService
     {
+        ITestimonialDal _testimonialDal;
 
+        public TestimonialManager(ITestimonialDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+
+        public Testimonial GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Testimonial> GetList()
+        {
+            return _testimonialDal.GetList();
+        }
+
+        public void TAdd(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
